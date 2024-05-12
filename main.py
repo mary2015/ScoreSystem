25d331dcce60c038d36a587943afc67666107989
MaRongyao <you@example.com>
2024-05-13 00:29:03 +0800

    update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import pytest
 
 if __name__ == '__main__':
     pytest.main(['-v', '--alluredir', './allure-result', './cases', '--clean-alluredir'])
-    os.system('/opt/homebrew/bin/allure generate ./allure-result/ - o ./allure-report/ --clean')
+    os.system('/opt/homebrew/bin/allure generate ./allure-result/ -o ./allure-report/ --clean')
