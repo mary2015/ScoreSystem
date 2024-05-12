d98c4043185f30e4c474c54e7f32ba70cf27441d
MaRongyao <you@example.com>
2024-05-13 03:02:19 +0800

    update ./allure-result to ./allure-results

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(['-v', '--alluredir', './allure-result', './cases', '--clean-alluredir'])
-    os.system('/opt/homebrew/bin/allure generate ./allure-result/ -o ./allure-report/ --clean')
+    pytest.main(['-v', '--alluredir', './allure-results', './cases', '--clean-alluredir'])
+    os.system('/opt/homebrew/bin/allure generate ./allure-results/ -o ./target/allure-report/ --clean')
